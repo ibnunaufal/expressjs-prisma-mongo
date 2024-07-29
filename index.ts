cc78782d3bc41876b59db205ce67b05d1e608df1
ibnu naufal <ibnunaufal5758@gmail.com>
2024-07-29 11:27:40 +0700

    chore: Add .vscode and .DS_Store to .gitignore, update package.json scripts

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
+import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
 import express from "express";
 
 const app = express();
 const PORT = 5000;
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,3 +12,26 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    prisma.user.findMany().then((users: any) => {
+        res.json(users);
+    }
+    ).catch((error: any) => {
+        res.json(error);
+    })
+});
+
+app.post("/add", (req, res) => {
+    const { name, email } = req.body;
+    prisma.user.create({
+        data: {
+            name,
+            email
+        }
+    }).then((user: any) => {
+        res.json(user);
+    }).catch((error: any) => {
+        res.json(error);
+    })
+})
